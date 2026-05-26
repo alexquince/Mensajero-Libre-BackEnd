3a46ff0d645a781df879107ff9fd4c5cda291a3d
david <cesardavesuarez@gmail.com>
2026-05-26 00:02:17 -0500

    Modules With no errors

---
+++ src/Modules/Turnos/dto/checkin-turno.dto.ts
@@ -0,0 +1,13 @@
+import { IsNumber, Min, Max } from 'class-validator';
+
+export class CheckInDto {
+  @IsNumber()
+  @Min(-90)
+  @Max(90)
+  latitud!: number;
+
+  @IsNumber()
+  @Min(-180)
+  @Max(180)
+  longitud!: number;
+}
